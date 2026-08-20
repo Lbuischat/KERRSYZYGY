81c778df66bd44604d05d55bd594791c5504fc7e
luí <luisadnzd@gmail.com>
2026-08-20 15:40:36 -0300

    Define routes for the game feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { GameShell } from './features/game/game-shell/game-shell';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'game',
+        component: GameShell
+    }
+];
