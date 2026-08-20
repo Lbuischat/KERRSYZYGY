dc3143e8f8fdb48acfda1d8a1e9c0d3132ce0467
luí <luisadnzd@gmail.com>
2026-08-20 15:41:03 -0300

    Add ActionBar component for combat feature

---
+++ src/app/features/game/combat/action-bar/action-bar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-action-bar',
+  styleUrl: './action-bar.css',
+  templateUrl: './action-bar.html',
+})
+export class ActionBar {}
