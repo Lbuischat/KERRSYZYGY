80eae33b5c9e8948c4131627bc4e15bbfc1714b4
luí <luisadnzd@gmail.com>
2026-08-20 15:42:53 -0300

    Add TaskPanel component with HTML, CSS, and unit tests

---
+++ src/app/features/tasks/task-panel/task-panel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-task-panel',
+  styleUrl: './task-panel.css',
+  templateUrl: './task-panel.html',
+})
+export class TaskPanel {}
