abf1434328f98475166f93f309f3589a3bea4c00
luí <luisadnzd@gmail.com>
2026-08-20 15:42:26 -0300

    Add MiniMap component with HTML, CSS, and unit tests

---
+++ src/app/features/game/hud/mini-map/mini-map.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-mini-map',
+  styleUrl: './mini-map.css',
+  templateUrl: './mini-map.html',
+})
+export class MiniMap {}
