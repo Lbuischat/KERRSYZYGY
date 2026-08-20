d640d43ac36c62657112f4f8909ba0abba057c77
luí <luisadnzd@gmail.com>
2026-08-20 16:55:02 -0300

    Add GameWorld component to app-game-shell imports

--- src/app/features/game/game-shell/game-shell.ts
+++ src/app/features/game/game-shell/game-shell.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { Hud } from '../hud/hud';
+import { GameWorld } from '../game-world/game-world';
 
 @Component({
-  imports: [Hud],
+  imports: [Hud, GameWorld],
   selector: 'app-game-shell',
   styleUrl: './game-shell.css',
   templateUrl: './game-shell.html',
