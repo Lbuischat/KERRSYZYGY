a2f77b047fc366ec88e1ff20c23ad44217e8ae70
luí <luisadnzd@gmail.com>
2026-08-20 15:42:37 -0300

    Add PlayerInfo component with HTML, CSS, and unit tests

---
+++ src/app/features/game/hud/player-info/player-info.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-player-info',
+  styleUrl: './player-info.css',
+  templateUrl: './player-info.html',
+})
+export class PlayerInfo {}
